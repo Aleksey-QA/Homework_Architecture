a1340003c992c1a371c2589e13cfa51db7a6a2c7
Aleksey-QA <alekseykotovai@gmail.com>
2026-04-23 22:40:02 +0300

    Changed step Allure

--- pages/boards_page.py
+++ pages/boards_page.py
@@ -17,14 +17,14 @@ class BoardsPage(BasePage):
 
         super().__init__(driver, self.url)
 
-    @allure.sub_suite('Проверка открытия и отображения board страницы')
+    @allure.step('Проверка открытия и отображения board страницы')
     def assert_that_boards_opened(self):
         self.assert_that_page_opened()
         self.assert_element_visible(self.BOARDS_H1)
         self.assert_element_visible(self.BOARDS_CREATE_BUTTON)
         self.assert_element_visible(self.BOARDS_TABLES)
 
-    @allure.sub_suite('Проверка наличия досок у пользователя и отсутcвие')
+    @allure.step('Проверка наличия досок у пользователя и отсутcвие')
     def assert_test_user_have_board(self):
         row = self.driver.find_elements(*self.BOARDS_COUNT_ROW)
         count = len(row)
